1363e871febc19b49c68531f30bd0efdcd31f419
jamestthompson3 <jamestthompson3@gmail.com>
2017-12-06 16:53:30 +0200

    [*] Minor refactoring

--- socket.js
+++ socket.js
@@ -2,11 +2,9 @@ const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
-app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`))
-
 io.on('connection', socket => {
   console.log(socket.id)
-  socket.on('distance', coords => console.log('distance detected ', coords))
+  socket.on('distance', distance => console.log('distance detected ', distance))
 })
 
 
